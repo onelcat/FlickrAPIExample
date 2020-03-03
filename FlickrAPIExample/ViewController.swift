ebd88f3c9f69bf043317bf3311df6f2ae70bebe3
haoshuai <haoshuai9192@gmail.com>
2020-03-03 17:20:27 +0800

    Initial Commit

---
+++ FlickrAPIExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrAPIExample
+//
+//  Created by haoshuai on 2020/3/3.
+//  Copyright © 2020 haoshuai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
